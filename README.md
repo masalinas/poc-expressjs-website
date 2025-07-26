eb2b9be343d97be60526476f87c7514271717d76
Miguel Angel Salinas Gancedo <masalinas.gancedo@gmail.com>
2025-07-26 16:58:53 +0200

    last commit

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A Simple configurable ExpressJS Web Server to check any Website. The script argu
 * path: folder path argument where exist static resources for the Website to be checked. Required
 
 # execute
-node . --port=8080 --path=/Users/masalinas/git/edikal-site
+node . --port=8080 --folder=/Users/masalinas/git/edikal-site
--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const app = express();
 // recover arguments
 const args = require('minimist')(process.argv.slice(2));
 const port = args['port']
-const path = args['path']
+const folder = args['folder']
 
-// configure default port argument and root path
+// configure default port argument and root folder
 if (port == undefined)
   port = 3000
 
-if (path == undefined) {
-  console.error('The path must be defined!')
+if (folder == undefined) {
+  console.error('The folder must be defined!')
   return;
 }
 
 // configure expressJS server
 app.use('/',
-  express.static(path),
-  serveIndex(path, { icons: true }));
+  express.static(folder),
+  serveIndex(folder, { icons: true }));
 
 // start expressJS Server
 app.listen(port);
 
-console.log('Listening on port:' + port + ' for site path:' + path);
+console.log('Listening on port:' + port + ' for site folder:' + folder);
